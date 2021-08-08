227d80b7c2d6f31582d87f8bce3ce7b2837f1c2c
Pablo Asenjo Navas-Parejo <pabloanp14@gmail.com>
2021-08-09 01:31:12 +0200

    CAMBIOS
    CAMBIOS

--- Main.cpp
+++ Main.cpp
@@ -121,6 +121,15 @@ private:
     vector<Substituent> substituents;
     unsigned short used_bonds;
     
+     string replaceAll(std::string str, const std::string& from, const std::string& to) {
+        size_t start_pos = 0;
+        while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+            str.replace(start_pos, from.length(), to);
+            start_pos += to.length(); 
+        }
+        return str;
+    }
+    
     string replaceAll(std::string str, const std::string& from, const std::string& to) {
         size_t start_pos = 0;
         while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
