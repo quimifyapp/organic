5fa889817734e862722085d0e2e1c3738e2c6972
Pablo Asenjo Navas-Parejo <pabloanp14@gmail.com>
2021-08-09 01:27:23 +0200

    Cambios
    Cambios

--- Main.cpp
+++ Main.cpp
@@ -55,7 +55,7 @@ private:
                   CH3 */
 
 public:
-    Substituent(){}
+    Substituent() {}
     Substituent(Id new_function, unsigned short new_bonds) {
         function = new_function;
         bonds = new_bonds;
@@ -120,6 +120,15 @@ class Carbon {
 private:
     vector<Substituent> substituents;
     unsigned short used_bonds;
+    
+    string replaceAll(std::string str, const std::string& from, const std::string& to) {
+        size_t start_pos = 0;
+        while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+            str.replace(start_pos, from.length(), to);
+            start_pos += to.length(); 
+        }
+        return str;
+    }
 
     string replaceAll(std::string str, const std::string& from, const std::string& to) {
         size_t start_pos = 0;
