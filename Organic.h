1dd3902e9c93d7b57b9f91977aac320f00d4ffad
Pablo Asenjo Navas-Parejo <pabloanp@outlook.com>
2021-08-29 20:37:41 +0200

    .

--- Organic.h
+++ Organic.h
@@ -26,51 +26,15 @@ public:
 
     Substituent(unsigned short new_carbons, bool new_iso);
 
-    Id getFunction() const;
     unsigned short getBonds() const;
-    unsigned short getCarbons() const;
-    bool getIso() const;
 };
 
-namespace sbts {
-    Substituent acid(Id::acid, 3);
-    Substituent carboxyl(Id::carboxyl, 1);
-    Substituent amide(Id::amide, 3);
-    Substituent carbamoyl(Id::carbamoyl, 1);
-    Substituent nitrile(Id::nitrile, 3);
-    Substituent cyanide(Id::cyanide, 1);
-    Substituent aldehyde(Id::aldehyde, 3);
-    Substituent ketone(Id::ketone, 2);
-    Substituent alcohol(Id::alcohol, 1);
-    Substituent amine(Id::amine, 1);
-    Substituent nitro(Id::nitro, 1);
-    Substituent bromine(Id::bromine, 1);
-    Substituent chlorine(Id::chlorine, 1);
-    Substituent fluorine(Id::fluorine, 1);
-    Substituent iodine(Id::iodine, 1);
-    Substituent hydrogen(Id::hydrogen, 1);
+namespace sbts{
+    Substituent acid, carboxyl, amide, carbamoyl, nitrile, 
+        cyanide, ketone, alcohol, amine, nitro, bromine, 
+        chlorine, fluorine, iodine, hydrogen, methyl;
 
-    Substituent methyl(1, false);
-    //Handy methyl constant
-
-    const map<Id, Substituent> list = {
-        {Id::acid, acid},
-        {Id::carboxyl, carboxyl},
-        {Id::amide, amide},
-        {Id::carbamoyl, carbamoyl},
-        {Id::nitrile, nitrile},
-        {Id::cyanide, cyanide},
-        {Id::aldehyde, aldehyde},
-        {Id::ketone, ketone},
-        {Id::alcohol, alcohol},
-        {Id::amine, amine},
-        {Id::nitro, nitro},
-        {Id::bromine, bromine},
-        {Id::chlorine, chlorine},
-        {Id::fluorine, fluorine},
-        {Id::iodine, iodine},
-        {Id::hydrogen, hydrogen}
-    };
+    const map<Id, Substituent> list;
 }
 
 class Carbon;
