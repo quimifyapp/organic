26a0c4d6c027a62c6bee8c79e1b230d3f9d15bf6
Pablo Asenjo Navas-Parejo <pabloanp@outlook.com>
2022-07-23 05:37:17 +0200

    Poca cosa

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,8 +1,8 @@
 class Main {
 
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo!");
     }
 
 }
