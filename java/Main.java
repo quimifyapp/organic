4e46fd78d1083a14facd70dc8b3a67ceab9e64aa
Pablo Asenjo Navas-Parejo <pabloanp@outlook.com>
2022-07-26 12:49:22 +0200

    PubChemPy

--- java/Main.java
+++ java/Main.java
@@ -1,12 +1,10 @@
 class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hola mundo!");
     }
 
-    // Interfaz para crear los compuestos desde la molécula
-    // Obtener la fórmula (String)
-    // Nombrar la fórmula usando la librería
-    // Traducir el nombre
+    // TODO: "but-1-eno" -> "but-1-ene" -> OPSIN -> "C=CCC" -> "CH2=CH3-CH3-CH3"
+    // TODO: "CH2=CH3-CH3-CH3" -> ? -> "but-1-eno"
+
 }
---
+++ lib/pubchempy/main.py
@@ -0,0 +1,7 @@
+import pubchempy as pcp
+
+a = pcp.Compound.from_cid(5090)
+b = pcp.get_compounds('C=C-C-C', 'smiles')
+
+#print(a.iupac_name)
+print(b[0].cid, b[0].iupac_name)
