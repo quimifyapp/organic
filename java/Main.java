dacddb9c37ba74d2991b7c58d948011213e222ff
Pablo Asenjo Navas-Parejo <pabloanp@outlook.com>
2022-07-26 10:22:23 +0200

    Carpetas

---
+++ java/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundo!");
+    }
+
+    // Interfaz para crear los compuestos desde la molécula
+    // Obtener la fórmula (String)
+    // Nombrar la fórmula usando la librería
+    // Traducir el nombre
+}
--- java/src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-
-    public static void main(String[] args)
-    {
-        System.out.println("Hola mundo!");
-    }
-
-}
