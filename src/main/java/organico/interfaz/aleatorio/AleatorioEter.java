ac3136e35a8c40453ddc1b66c132804315ea1fec
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2022-08-09 02:41:52 +0200

    Ajustes de OPSIN, y AleatorioEter

--- src/main/java/organico/interfaz/aleatorio/AleatorioEter.java
+++ src/main/java/organico/interfaz/aleatorio/AleatorioEter.java
@@ -4,22 +4,25 @@ import organico.componentes.Id;
 import organico.componentes.Sustituyente;
 import organico.intermediarios.opsin.Opsin;
 import organico.intermediarios.opsin.OpsinResultado;
+import organico.tipos.Eter;
 import organico.tipos.Simple;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class AleatorioSimple {
+public class AleatorioEter {
 
+	private static final Scanner scanner = new Scanner(System.in);
 	private static List<Integer> secuencia;
 
 	public static void main(String[] args) {
 		while(true) {
-			Simple simple = getSimpleAleatorio();
+			Eter eter = getEterAleatorio();
 
-			simple.corregir(); // Es necesario
-			String nombre = simple.getNombre();
+			eter.corregir(); // Es necesario
+			String nombre = eter.getNombre();
 
 			OpsinResultado opsin_resultado = Opsin.procesarNombreES(nombre);
 
@@ -32,26 +35,26 @@ public class AleatorioSimple {
 		}
 	}
 
-	private static Simple getSimpleAleatorio() {
-		Simple simple = new organico.tipos.Simple();
+	private static Eter getEterAleatorio() {
+		Eter eter = new Eter();
 
 		List<Integer> elecciones = new ArrayList<>();
 		boolean primer_carbono = true;
 
-		while(!simple.estaCompleta()) {
-			List<Id> disponibles = simple.getSustituyentesDisponibles();
+		while(!eter.estaCompleta()) {
+			List<Id> disponibles = eter.getSustituyentesDisponibles();
 
-			int eleccion = ThreadLocalRandom.current().nextInt(0, 2) == 0 && !primer_carbono
+			int eleccion = ThreadLocalRandom.current().nextInt(0, 5) == 0 && !primer_carbono
 					? 0
 					: (ThreadLocalRandom.current().nextInt(0, 2) == 0
-						? ThreadLocalRandom.current().nextInt(1, disponibles.size())
+						? (disponibles.size() > 1 ? ThreadLocalRandom.current().nextInt(1, disponibles.size()) : 1)
 						: disponibles.size() - 1);
 			elecciones.add(eleccion);
 
 			if(eleccion == 0 && !primer_carbono)
-				simple.enlazarCarbono();
+				eter.enlazarCarbono();
 			else if(disponibles.get(eleccion - 1) != Id.radical)
-				simple.enlazar(disponibles.get(eleccion - 1));
+				eter.enlazar(disponibles.get(eleccion - 1));
 			else {
 				int carbonos = ThreadLocalRandom.current().nextInt(3, 5);
 				if(eleccion == 1)
@@ -59,7 +62,7 @@ public class AleatorioSimple {
 				else elecciones.add(0);
 				elecciones.add(carbonos);
 
-				simple.enlazar(new Sustituyente(carbonos, eleccion == 1));
+				eter.enlazar(new Sustituyente(carbonos, eleccion == 1));
 			}
 
 			if(primer_carbono)
@@ -68,7 +71,7 @@ public class AleatorioSimple {
 
 		secuencia = elecciones;
 
-		return simple;
+		return eter;
 	}
 
 }
--- src/main/java/organico/intermediarios/opsin/Opsin.java
+++ src/main/java/organico/intermediarios/opsin/Opsin.java
@@ -8,18 +8,34 @@ public class Opsin {
     protected static final NameToStructure opsin_en = NameToStructure.getInstance();
 
     public static OpsinResultado procesarNombreES(String nombre) {
-        // Nuestra adaptación al español de OPSIN rechaza el prefijo "ácido", por eso se elimina:
-        nombre = nombre.replaceFirst("ácido ", "");
+        // Nuestra adaptación al español de la librería OPSIN rechaza el prefijo "ácido", por eso se elimina:
+        nombre = nombre.replaceFirst("ácido|acido", "");
+
+        // La librería OPSIN rechaza el prefijo "di" de los éteres simétricos en algunos casos:
+        nombre = corregirEter(nombre);
 
         es.opsin.OpsinResult opsin_result = opsin_es.parseChemicalName(nombre);
 
         return new OpsinResultado(opsin_result);
     }
-
     public static OpsinResultado procesarNombreEN(String nombre) {
+        nombre = corregirEter(nombre);
+
         OpsinResult opsin_result = opsin_en.parseChemicalName(nombre);
 
         return new OpsinResultado(opsin_result);
+    }
+
+    private static String corregirEter(String nombre) {
+        if(nombre.contains("éter") || nombre.contains("eter") || nombre.contains("ether")) {
+            String arreglado = nombre.replaceFirst("di", "");
+
+            // Puede que el "di" no sea porque es simétrico, sino que sea el cuantificador de un sustituyente:
+            if(arreglado.trim().split("\\s+").length == 2) // Como "etil éter"
+                nombre = arreglado;
+        }
+
+        return nombre;
     }
 
 }
