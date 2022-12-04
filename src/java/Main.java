bcf7d877e5a3e00a4e28dbb16642e49f380e9696
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2022-12-04 12:35:44 +0100

    Main loop

--- src/java/Main.java
+++ src/java/Main.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String name;
+        while(true) {
+            String name;
 
-        //name = "etil metil eter";
-        //name = "ácido 4-fluoro-2-oxobutanoico";
-        //name = "ácido 3-butil-4-fluoro-2-oxobutanoico";
-        //name = "ácido 3-isobutil-4-fluoro-2-oxobutanoico";
-        //name = "ácido 3-terbutil-4-fluoro-2-oxobutanoico";
-        //name = "ciclopentano";
+            //name = "etil metil eter";
+            //name = "ácido 4-fluoro-2-oxobutanoico";
+            //name = "ácido 3-butil-4-fluoro-2-oxobutanoico";
+            //name = "ácido 3-isobutil-4-fluoro-2-oxobutanoico";
+            //name = "ácido 3-terbutil-4-fluoro-2-oxobutanoico";
+            //name = "ciclopentano";
 
-        name = new Scanner(System.in).nextLine();
+            name = new Scanner(System.in).nextLine();
 
-        System.out.println(OrganicFactory.getFromName(name).getStructure());
+            System.out.println(OrganicFactory.getFromName(name).getStructure());
+        }
     }
 
 }
