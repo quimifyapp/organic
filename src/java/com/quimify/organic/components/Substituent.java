6de0160fd32829f8fc8d9dab3181680eec1966f6
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2022-12-11 01:50:01 +0100

    Bug solved: overrided hashCode for Substituent, which fixed Set-related bugs

--- src/java/com/quimify/organic/components/Substituent.java
+++ src/java/com/quimify/organic/components/Substituent.java
@@ -3,6 +3,7 @@ package com.quimify.organic.components;
 import com.quimify.organic.Organic;
 
 import java.util.Map;
+import java.util.Objects;
 
 import static java.util.Map.entry;
 
@@ -104,6 +105,11 @@ public class Substituent extends Organic {
     }
 
     // Queries:
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(group, bondCount, carbonCount, isIso);
+    }
 
     @Override
     public boolean equals(Object other) {
