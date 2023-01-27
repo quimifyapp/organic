ebc5bf5c537960ef2e9edcbb809a4963df78b887
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-01-27 22:27:10 +0100

    Broke Carbon.getStructure into multiple methods

--- src/main/java/com/quimify/organic/components/Carbon.java
+++ src/main/java/com/quimify/organic/components/Carbon.java
@@ -107,50 +107,75 @@ public class Carbon extends Nomenclature {
     // Text:
 
     private String getStructure() {
-        StringBuilder result = new StringBuilder("C");
+        // "CHCl2-O-" = "C" + "H" + "Cl2" + "-O-"
+        return "C" + getHydrogenStructure() + getOtherSubstituentsStructure() + getEtherStructure();
+    }
 
-        // Hydrogen:
+    private String getHydrogenStructure() {
+        int hydrogenCount = getAmountOf(Group.hydrogen);
 
-        final int hydrogenCount = getAmountOf(Group.hydrogen);
+        if(hydrogenCount == 0)
+            return "";
 
-        if(hydrogenCount > 0)
-            result.append(new Substituent(Group.hydrogen)).append(molecularQuantifierFor(hydrogenCount));
+        // "H", "H2", "H3"
+        return new Substituent(Group.hydrogen) + molecularQuantifierFor(hydrogenCount);
+    }
 
-        // Rest of them except ether:
+    private StringBuilder getOtherSubstituentsStructure() {
+        Stream<Substituent> uniqueSubstituents = substituents.stream()
+                .filter(s -> s.getGroup() != Group.hydrogen && s.getGroup() != Group.ether).distinct();
 
-        Set<Substituent> uniqueSubstituents = new HashSet<>(substituents);
-        uniqueSubstituents.removeIf(s -> s.getGroup() == Group.hydrogen || s.getGroup() == Group.ether);
-
-        List<Substituent> uniqueOrderedSubstituents = uniqueSubstituents.stream()
+        List<Substituent> uniqueOrderedSubstituents = uniqueSubstituents
                 .sorted(Substituent::compareTo).collect(Collectors.toList());
 
-        if(uniqueOrderedSubstituents.size() == 1) { // Only one kind except for hydrogen and ether
-            Substituent substituent = uniqueOrderedSubstituents.get(0);
-            Group group = substituent.getGroup();
+        StringBuilder otherSubstituentsStructure;
 
-            if(substituent.getBondCount() == 3 && group != Group.aldehyde)
-                result.append(substituent); // CHOOH, CONH2-...
-            else if(group == Group.aldehyde && hydrogenCount == 0)
-                result.append(substituent); // CHO
-            else if(group == Group.ketone && hydrogenCount == 0)
-                result.append(substituent); // -CO-
-            else if (Nomenclature.isHalogen(group))
-                result.append(substituent); // CHCl2, CF3-...
-            else result.append("(").append(substituent).append(")"); // CH(HO), CH(NO2)3, CH2(CH3)-...
+        if (uniqueOrderedSubstituents.size() == 1)
+            otherSubstituentsStructure = getSingleSubstituentStructure(uniqueOrderedSubstituents.get(0));
+        else otherSubstituentsStructure = getMultipleSubstituentsStructure(uniqueOrderedSubstituents);
 
-            result.append((molecularQuantifierFor(getAmountOf(substituent))));
+        return otherSubstituentsStructure;
+    }
+
+    private StringBuilder getSingleSubstituentStructure(Substituent substituent) {
+        StringBuilder otherGroupsStructure = new StringBuilder();
+
+        Group group = substituent.getGroup();
+        int hydrogenCount = getAmountOf(Group.hydrogen);
+
+        if (substituent.getBondCount() == 3 && group != Group.aldehyde)
+            otherGroupsStructure.append(substituent); // CHOOH, CONH2-...
+        else if (group == Group.aldehyde && hydrogenCount == 0)
+            otherGroupsStructure.append(substituent); // CHO
+        else if (group == Group.ketone && hydrogenCount == 0)
+            otherGroupsStructure.append(substituent); // -CO-
+        else if (Nomenclature.isHalogen(group))
+            otherGroupsStructure.append(substituent); // CHCl2, CF3-...
+        else otherGroupsStructure.append("(").append(substituent).append(")"); // CH(HO), CH(NO2)3, CH2(CH3)-...
+
+        otherGroupsStructure.append((molecularQuantifierFor(getAmountOf(substituent))));
+
+        return otherGroupsStructure;
+    }
+
+    private StringBuilder getMultipleSubstituentsStructure(List<Substituent> uniqueOrderedSubstituents) {
+        StringBuilder otherGroupsStructure = new StringBuilder();
+
+        // C(OH)3(Cl)
+        for (Substituent substituent : uniqueOrderedSubstituents) {
+            otherGroupsStructure.append("(").append(substituent).append(")"); // C(OH)
+            otherGroupsStructure.append(molecularQuantifierFor(getAmountOf(substituent))); // C(OH)3
         }
-        else if(uniqueOrderedSubstituents.size() > 1) // More than one kind except for hydrogen and ether
-            for (Substituent substituent : uniqueOrderedSubstituents)
-                result.append("(").append(substituent).append(")") // C(OH)3(Cl), CH2(NO2)(CH3)...
-                        .append(molecularQuantifierFor(getAmountOf(substituent)));
 
-        // Ether:
+        return otherGroupsStructure;
+    }
 
-        if(isBondedTo(Group.ether))
-            result.append(new Substituent(Group.ether)); // CHBr-O-
+    private String getEtherStructure() {
+        if(!isBondedTo(Group.ether))
+            return "";
 
-        return result.toString();
+        // -O-
+        return new Substituent(Group.ether).toString();
     }
 
     @Override
